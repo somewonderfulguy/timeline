505936734651bb46402f5f86235a87465fe53a3a
Dmitriy Yastrebov <dmitriy.yastrebov@globallogic.com>
2024-03-06 11:21:30 +0200

    implement preview

--- src/components/PlayerBlank/PlayerBar/PlayerBar.tsx
+++ src/components/PlayerBlank/PlayerBar/PlayerBar.tsx
@@ -1,3 +1,6 @@
+import { FaExpandAlt } from 'react-icons/fa';
+import { FaBackwardStep, FaPlay, FaForwardStep } from 'react-icons/fa6';
+
 import styles from './PlayerBar.module.css';
 
 const PlayerBar = () => {
@@ -7,10 +10,35 @@ const PlayerBar = () => {
         <div className={styles.progress} />
       </div>
       <div className={styles.controls}>
-        <button className={styles.controlButton}>Previous</button>
-        <button className={styles.controlButton}>Play</button>
-        <button className={styles.controlButton}>Next</button>
+        <button
+          type="button"
+          title="Step Back 1 Frame"
+          className={styles.controlButton}
+        >
+          <FaBackwardStep />
+        </button>
+        <button
+          type="button"
+          title="Play-Stop Toggle"
+          className={styles.controlButtonLarger}
+        >
+          <FaPlay />
+        </button>
+        <button
+          type="button"
+          title="Step Forward 1 Frame"
+          className={styles.controlButton}
+        >
+          <FaForwardStep />
+        </button>
       </div>
+      <button
+        type="button"
+        title="Full Screen View"
+        className={styles.expandButton}
+      >
+        <FaExpandAlt />
+      </button>
     </div>
   );
 };
